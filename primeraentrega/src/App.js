c965ff87a799e8ea08ca69abc0229a3e934a0098
juan sacco <juandsacco@gmail.com>
2022-12-02 14:23:56 -0300

    agrego repo primera entrega react

---
+++ primeraentrega/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+import CartasCompletas from './componentes/Laptop/Laptop';
+import Header from './componentes/NavBar/NavBar';
+
+
+
+export default function App() {
+  return (
+    <>
+    <div><Header/></div>
+    <div>
+      <CartasCompletas />
+    </div>
+    </>
+  );
+};
+
